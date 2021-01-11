d3c16eae4174ecb670226ea7e3ccf41143f76e58
Nitin Ranganath <nitinranganath@gmail.com>
2021-01-11 20:37:19 +0530

    Create custom error middleware

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
+const {
+  routeNotFound,
+  errorHandler,
+} = require('./middlewares/error.middleware');
 
 // Load environment variables
 dotenv.config();
@@ -12,6 +16,10 @@ connectDB();
 const app = express();
 
 app.get('/', (req, res) => res.send('API is up and running!'));
+
+// Handle errors
+app.use(routeNotFound);
+app.use(errorHandler);
 
 // Start up the server
 const PORT = process.env.PORT || 5000;
