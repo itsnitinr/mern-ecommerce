c15d0759d731a5d8c75e3cffbd07d03f07c60bd6
Nitin Ranganath <nitinranganath@gmail.com>
2021-01-11 20:20:56 +0530

    Build authorization middleware

---
+++ backend/middlewares/auth.middleware.js
@@ -0,0 +1,45 @@
+import jwt from 'jsonwebtoken';
+import asyncHandler from 'express-async-handler';
+import User from '../models/User.model';
+
+// Restrict routes to only logged in users using JWT
+const auth = asyncHandler(async (req, res, next) => {
+  let token;
+
+  // Check if token is sent in header
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    try {
+      // Get token from authorization string
+      token = req.headers.authorization.split(' ')[0];
+      // Verify if the token matches
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      // Get the logged in user from decoded token's id field
+      req.user = await User.findById(decoded.id).select('-password');
+      next();
+    } catch (error) {
+      res.status(401);
+      throw new Error('Unauthorized. Token invalid.');
+    }
+  }
+
+  if (!token) {
+    res.status(401);
+    throw new Error('Unauthorized. No token.');
+  }
+});
+
+// Restrict routes to only admin users
+const admin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    // Execute further if admin
+    next();
+  } else {
+    res.status(401);
+    throw new Error('You need admin rights to access this!');
+  }
+};
+
+module.exports = { auth, admin };
