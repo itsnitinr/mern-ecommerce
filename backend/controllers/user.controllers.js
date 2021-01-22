3b5892d36f036326c91c2a38f3a944ac3e31f9f2
Nitin Ranganath <nitinranganath@gmail.com>
2021-01-22 15:30:25 +0530

    Change reset password url to match frontend route

--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -89,7 +89,7 @@ const forgotPassword = asyncHandler(async (req, res) => {
 
   const resetUrl = `${req.protocol}://${req.get(
     'host'
-  )}/api/users/reset-password/${resetToken}`;
+  )}/reset-password/${resetToken}`;
 
   const message = `You are receiving this email because you (or someone else) has requested the reset of your password. Please make a PUT request to: \n\n ${resetUrl}`;
 
