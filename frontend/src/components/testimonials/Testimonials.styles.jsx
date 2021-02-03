b0ef101ae6c45af70787c800a493946a477cc6c1
Nitin Ranganath <nitinranganath@gmail.com>
2021-02-03 18:48:59 +0530

    Testimonials fix

--- frontend/src/components/testimonials/Testimonials.styles.jsx
+++ frontend/src/components/testimonials/Testimonials.styles.jsx
@@ -2,19 +2,26 @@ import { makeStyles } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   wrapper: {
-    overflow: 'hidden',
+    overflow: 'initial',
     width: '70vw',
     margin: '3rem auto',
     [theme.breakpoints.down('md')]: {
       width: '90vw',
+      overflow: 'hidden',
     },
+  },
+  testimonialCard: {
+    marginBottom: '2rem',
   },
   testimonialCardImage: {
     height: '100%',
     width: '100%',
     borderRadius: '10px 0 0 10px',
+    outline: 'none',
+    marginLeft: '2rem',
     [theme.breakpoints.down('xs')]: {
       borderRadius: '10px 10px 0 0',
+      marginLeft: 0,
     },
   },
   testimonialCardText: {
@@ -25,8 +32,12 @@ const useStyles = makeStyles((theme) => ({
     flexDirection: 'column',
     height: '100%',
     background: '#f4f4f4',
+    marginRight: '2rem',
     textAlign: 'center',
     borderRadius: '0 10px 10px 0',
+    [theme.breakpoints.down('xs')]: {
+      marginRight: 0,
+    },
   },
   quoteIcon: {
     fontSize: '6rem',
